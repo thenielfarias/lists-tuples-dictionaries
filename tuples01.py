046cb9dab41b6f6c12d646abff13dc1c6d82e7f8
Daniel Farias <dan.tfarias@gmail.com>
2021-07-06 20:50:54 -0300

    Update tuples01.py

--- tuples01.py
+++ tuples01.py
@@ -7,3 +7,5 @@ userNum = int(input("Digite um número entre 0 e 20: "))
 if userNum < 0 or userNum > 20:
     userNum = int(input("Número inválido, tente novamente. Digite um número: ")) 
 print('Você digitou o número {}'.format(nums[userNum]))
+
+
