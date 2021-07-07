344d8dac3ff29786c824b44c6e9fe249cf0230ce
Daniel Farias <dan.tfarias@gmail.com>
2021-07-07 10:58:03 -0300

    Add files via upload

---
+++ tuples05.py
@@ -0,0 +1,12 @@
+p = (['Lápis', '1.75'], ['Borracha', '2.00'], ['Caderno', '15.90'], ['Estojo', '25.00'], ['Transferidor', '4.20'],
+     ['Compasso', '9.99'], ['Mochila', '120.99'], ['Canetas', '22.30'], ['Livro', '34.90'])
+
+print('-'*38)
+print('{:^38}'.format('LISTAGEM DE PREÇOS'))
+print('-'*38)
+
+for el1, el2 in p:
+    print('{:.<30}{}{:''>7}'.format(el1, ('R$'), el2))
+
+print('-'*38)
+
