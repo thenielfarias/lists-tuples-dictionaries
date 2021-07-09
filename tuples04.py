f4318ca5476717ca1c0f2b3c7c693712c360ffca
Daniel Farias <dan.tfarias@gmail.com>
2021-07-09 12:24:47 -0300

    Update tuples04.py

--- tuples04.py
+++ tuples04.py
@@ -1,68 +1,24 @@
 while True:
     try:
-        n1 = int(input('Digite um número: '))
+        num = (int(input('Digite um número: ')),
+               int(input('Digite outro número: ')),
+               int(input('Digite mais um número: ')),
+               int(input('Digite o último número: ')))
         break
     except ValueError:
         print('Reposta inválida, tente novamente.')
 
-while True:
-    try:        
-        n2 = int(input('Digite mais um número: '))
-        break
-    except ValueError:
-        print('Reposta inválida, tente novamente.')      
 
-while True:
-    try:
-        n3 = int(input('Digite outro número: '))
-        break
-    except ValueError:
-        print('Reposta inválida, tente novamente.')      
-        
-while True:
-    try:             
-        n4 = int(input('Digite um último número: '))
-        break
-    except ValueError:
-        print('Reposta inválida, tente novamente.')
-        
-t = (n1, n2, n3, n4)
-
-cont9 = 0
-if t[0] == 9:
-    cont9 += 1
-if t[1] == 9:
-    cont9 += 1
-if t[2] == 9:
-    cont9 += 1
-if t[3] == 9:
-    cont9 += 1
-
-pares = []
-if t[0] % 2 == 0:
-    pares.append(t[0])
-if t[1] % 2 == 0:
-    pares.append(t[1])
-if t[2] % 2 == 0:
-    pares.append(t[2])
-if t[3] % 2 == 0:
-    pares.append(t[3])
-
-if pares != []:
-    print('Os números pares digitados foram: ')
-    for el in pares:
-        print(el, end='\n')
+print(f'Você digitou os números {num}')
+print(f'O valor 9 apareceu {num.count(9)} vez(es)')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
 else:
-    print('Não foram digitados números pares.')
-    
-try:
-    primeira_pos3 = t.index(3)
-except:
-    print('O número três não foi digitado.')
-
-print(f'O número nove foi digitado {cont9} vez(es)')
-print(f'O número três foi digitado primeiramente na posição {primeira_pos3}')
-
-
-
-
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
+        
+        
+        
