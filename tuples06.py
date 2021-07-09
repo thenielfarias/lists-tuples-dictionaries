07240d57ed31be6a3344751e356362d35e007789
Daniel Farias <dan.tfarias@gmail.com>
2021-07-09 12:43:01 -0300

    Update tuples06.py

--- tuples06.py
+++ tuples06.py
@@ -1,17 +1,9 @@
-t = ('violao', 'guitarra', 'bateria', 'baixo', 'teclado')
-cont = 0
-for elements in t:
-    print(f'Na palavra {t[cont]} temos as vogais:', end=' ')
-    if 'a' in t[cont]:
-        print('a', end=' ')
-    if 'e' in t[cont]:
-        print('e', end=' ')
-    if 'i' in t[cont]:
-        print('i', end=' ')
-    if 'o' in t[cont]:
-        print('o', end=' ')
-    if 'u' in t[cont]:
-        print('u', end=' ')
-    cont += 1
-    print('\n')
+instruments = ('violao', 'guitarra', 'bateria', 'baixo', 'teclado')
+for words in instruments:
+    print(f'\nNa palavra {words.upper()} temos as vogais:', end=' ')
+    for letters in words:
+        if letters.lower() in 'aeiou':
+            print(letters, end=' ')
+            
+            
  
