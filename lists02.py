a2b06aa7418907ce7c21e5bc879d8b5ad6b0fa30
Daniel Farias <dan.tfarias@gmail.com>
2021-07-12 09:21:46 -0300

    Add files via upload

---
+++ lists02.py
@@ -0,0 +1,13 @@
+números = []
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in números:
+        números.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado')
+    r = str(input('Quer continuar? [S/N]'))
+    if r in 'Nn':
+        break
+print('-='*20)
+print(f'Você digitou os valores únicos {números}')
