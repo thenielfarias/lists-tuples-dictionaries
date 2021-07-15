4791bbe964df3f1fee8d1d3cd42c0d9720acc956
Daniel Farias <dan.tfarias@gmail.com>
2021-07-15 12:09:50 -0300

    Add files via upload

---
+++ lists08.py
@@ -0,0 +1,11 @@
+lista7 = [[], []]
+valor = 0
+for el in range(1, 8):
+    valor = int(input(f'Digite o {el}o. valor: '))
+    if valor % 2 == 0:
+        lista7[0].append(el)
+    else:
+        lista7[1].append(el)
+
+print(f'Os valores pares digitados foram: {lista7[0]}')
+print(f'Os valores ímpares digitados foram: {lista7[1]}')
