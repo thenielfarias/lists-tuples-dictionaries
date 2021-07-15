06fa2ba62c455d4e0038db8e6b2b14ff43f64a54
Daniel Farias <dan.tfarias@gmail.com>
2021-07-15 19:04:34 -0300

    Update lists09.py

--- lists09.py
+++ lists09.py
@@ -1,4 +1,15 @@
-matriz = [[], [], []]
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-'*30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+
+'''matriz = [[], [], []]
 
 c0 = c1 = c2 = 0
 while True:
@@ -16,6 +27,6 @@ while True:
 print('-'*30)
 print(f'[ {matriz[0][0]} ] [ {matriz[0][1]} ] [ {matriz[0][2]} ]')
 print(f'[ {matriz[1][0]} ] [ {matriz[1][1]} ] [ {matriz[1][2]} ]')
-print(f'[ {matriz[2][0]} ] [ {matriz[2][1]} ] [ {matriz[2][2]} ]')
+print(f'[ {matriz[2][0]} ] [ {matriz[2][1]} ] [ {matriz[2][2]} ]')'''
 
 
