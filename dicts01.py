bb2413b168dcf13529d56f683d2de35a649cc0d2
Daniel Farias <dan.tfarias@gmail.com>
2021-07-17 14:29:48 -0300

    Update dicts01.py

--- dicts01.py
+++ dicts01.py
@@ -1,7 +1,7 @@
 aluno = dict()
 for c in range(0, 1):
-    aluno['nome'] = str(input('Nome: '))
-    aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+    aluno["nome"] = str(input('Nome: '))
+    aluno["media"] = float(input(f'Média de {aluno["nome"]}: '))
 print(f'O nome do aluno é {aluno["nome"]}')
 print(f'A média é {aluno["media"]}')
 if aluno["media"] >= 7:
