92551120838d00e77e16fbfab5a4ba1f83291ad3
Daniel Farias <dan.tfarias@gmail.com>
2021-07-17 14:28:19 -0300

    Add files via upload

---
+++ dicts01.py
@@ -0,0 +1,13 @@
+aluno = dict()
+ficha = list()
+for c in range(0, 1):
+    aluno['nome'] = str(input('Nome: '))
+    aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+print(f'O nome do aluno é {aluno["nome"]}')
+print(f'A média é {aluno["media"]}')
+if aluno["media"] >= 7:
+    print('A situação é APROVADO')
+else:
+    print('A situação é REPROVADO')
+
+
